cc7f472ca925676f48b9e338b661c80fce846e8c
adno88 <adno88@o2.pl>
2023-01-31 18:07:04 +0100

    Added working turn counter

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ const c0 = document.getElementById('c0');
 const c1 = document.getElementById('c1');
 const c2 = document.getElementById('c2');
 const c3 = document.getElementById('c3');
-
 const c4 = document.getElementById('c4');
 const c5 = document.getElementById('c5');
 const c6 = document.getElementById('c6');
 const c7 = document.getElementById('c7');
-
 const c8 = document.getElementById('c8');
 const c9 = document.getElementById('c9');
 const c10 = document.getElementById('c10');
@@ -19,23 +17,32 @@ c0.addEventListener("click", function() {uncover(0);});
 c1.addEventListener("click", function() {uncover(1);});
 c2.addEventListener("click", function() {uncover(2);});
 c3.addEventListener("click", function() {uncover(3);});
-
 c4.addEventListener("click", function() {uncover(4);});
 c5.addEventListener("click", function() {uncover(5);});
 c6.addEventListener("click", function() {uncover(6);});
 c7.addEventListener("click", function() {uncover(7);});
-
 c8.addEventListener("click", function() {uncover(8);});
 c9.addEventListener("click", function() {uncover(9);});
 c10.addEventListener("click", function() {uncover(10);});
 c11.addEventListener("click", function() {uncover(11);});
 
-const firstVisible = false;
-const turn = 0;
+var firstVisible = false;
+var turn = 0;
 
 function uncover(nr) {
     var image = "url(image/"+cards[nr]+")";
     $('#c'+nr).css('background-image', image);
+    $('#c'+nr).addClass('uncoverCard');
+    $('#c'+nr).removeClass('card');
+
+    if(firstVisible == false) {
+        firstVisible = true;
+    }
+    else {
+        turn++;
+        $('.score').html('Turns: '+turn);
+        firstVisible = false;
+    }
 }
 
 /* const card = document.querySelectorAll('.card');
